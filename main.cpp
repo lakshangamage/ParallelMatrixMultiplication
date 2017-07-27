8bff14d4a5bac2088d0bddc16dd06bb072f297be
Lakshan Gamage <lakshangamage.13@cse.mrt.ac.lk>
2017-07-27 13:14:25 +0530

    completed running many samples

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@
 #include <stdlib.h>     
 #include <time.h> 	
 #include <omp.h>
+#include <math.h> 
 #define CHUNKSIZE 100	
+#define MAX_SAMPLE_SIZE 50	
 #define GAP 200	
+#define CONFIDENCE 1.96	
+#define ACCURACY 0.05	
 using namespace std;
-
-int nthreads, tid;
 
 int N = 200;
 int max_N;
 double *matrixA;
 double *matrixB;
 
-double start_time;
-double end_time;
-double time_spent;
-
 void printMatrix(double *matrix);
 double* multiplyMatrices(double *matrix_1, double *matrix_2);
 double* multiplyMatricesParallel(double *matrix_1, double *matrix_2);
+double calcStd(double* totArrray, double mean, int noOfSamples);
+double calcSampleCount(double std);
 
 int main ()
 {
@@ -30,52 +30,92 @@ int main ()
 	getline(cin, user_input);
 	stringstream(user_input) >> max_N;
 
+
 	while(N <= max_N) {
-
+		int currentSample = 1;
+		int samplesNeeded = MAX_SAMPLE_SIZE;
+		double *serialTotArray = new double[MAX_SAMPLE_SIZE];
+		double serial_tot = 0 ;
+		double serial_mean = 0;
+		double *parallelTotArray = new double[MAX_SAMPLE_SIZE];
+		double parallel_tot = 0;
+		double parallel_mean = 0;
+		while( samplesNeeded <= MAX_SAMPLE_SIZE && samplesNeeded > currentSample){
 		//int array_size = N;
-		matrixA = new double[N*N];
-		matrixB  = new double[N*N];
+			matrixA = new double[N*N];
+			matrixB  = new double[N*N];
 
-	  /* initialize random seed: */
-		srand (time(NULL));
+		  /* initialize random seed: */
+			srand (time(NULL));
 
-		for(int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++)
+			for(int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++)
+				{
+					matrixA[i*N+j] = rand();
+					matrixB[i*N+j] = rand();  		
+				}  	
+			}
+			// printMatrix(matrixA);
+			// printMatrix(matrixB);
+
+			//cout << "\n==============Sequential===========\n"<< endl;
+			double serial_time = omp_get_wtime();
+
+			double *resultMatrix = multiplyMatrices(matrixA, matrixB);
+
+			double serial_time_spent = (omp_get_wtime() - serial_time);
+			
+			delete []resultMatrix;
+
+			//cout << "\n==============Parallel===========\n"<< endl;
+			double parallel_time = omp_get_wtime();
+
+			double *parallelResultMatrix = multiplyMatricesParallel(matrixA, matrixB);
+			//cout << "clock end" << endl;
+			double parallel_time_spent = (omp_get_wtime() - parallel_time);
+
+			//printMatrix(parallelResultMatrix);
+
+			delete []parallelResultMatrix;
+			delete []matrixA; 
+			delete []matrixB;
+
+			serial_tot += serial_time_spent;
+			serialTotArray[currentSample-1] = serial_time_spent;
+			
+			
+			parallel_tot += parallel_time_spent;
+			parallelTotArray[currentSample-1] = parallel_time_spent;
+			
+
+			if (currentSample > 1)
 			{
-				matrixA[i*N+j] = rand();
-				matrixB[i*N+j] = rand();  		
-			}  	
+				serial_mean = serial_tot/currentSample;
+				double serialStd = calcStd(serialTotArray, serial_mean, currentSample);
+				int serialSampleCount = calcSampleCount(serialStd);
+
+				parallel_mean = parallel_tot / currentSample;
+				double parallelStd = calcStd(parallelTotArray, parallel_mean, currentSample);
+				int parallelSampleCount = calcSampleCount(parallelStd);
+
+				samplesNeeded = serialSampleCount;
+				if (serialSampleCount < parallelSampleCount)
+				{
+					samplesNeeded = parallelSampleCount;
+				}
+			}
+
+			currentSample++;
 		}
-		// printMatrix(matrixA);
-		// printMatrix(matrixB);
 
-		//cout << "\n==============Sequential===========\n"<< endl;
-		start_time = omp_get_wtime();
+		cout << "Matrix size              = " << N << endl;
+		cout << "No. of Samples           = " << currentSample - 1 << endl;
+		cout << "Average time sequential  = " << serial_mean <<" s" << endl;		
+		cout << "Average time parallel    = " << parallel_mean <<" s"<< endl ;
+		cout << "Speedup Achieved         = " << serial_mean / parallel_mean << endl<< endl;
 
-		double *resultMatrix = multiplyMatrices(matrixA, matrixB);
-		end_time = omp_get_wtime();
-
-		time_spent = (end_time - start_time) * 1000.0;
-
-		cout << "Matrix size = " << N << endl;
-		cout << "Total time spent for sequential multiplication = " << time_spent <<" ms" << endl;
-		delete []resultMatrix;
-
-		//cout << "\n==============Parallel===========\n"<< endl;
-		start_time = omp_get_wtime();
-
-		double *parallelResultMatrix = multiplyMatricesParallel(matrixA, matrixB);
-		//cout << "clock end" << endl;
-		end_time = omp_get_wtime();
-		time_spent = (end_time - start_time) * 1000.0;
-
-		cout << "Total time spent for parallel multiplication = " << time_spent <<" ms"<< endl << endl;
-
-		//printMatrix(parallelResultMatrix);
-
-		delete []parallelResultMatrix;
-		delete []matrixA; 
-		delete []matrixB;
+		delete []serialTotArray;
+		delete []parallelTotArray;
 		N += GAP;
 	} 
 	 
@@ -137,7 +177,16 @@ double* multiplyMatricesParallel(double *matrix_1, double *matrix_2){
 	return outputMatrix;
 } 
 
+double calcStd(double* totArrray, double mean, int noOfSamples){
+	double total = 0;
+	for (int i = 0; i < noOfSamples; i++)
+	{
+		total += pow((totArrray[i] - mean),2.0);
+	}
+	return sqrt(total/noOfSamples);
+}
 
-
-
+double calcSampleCount(double std){
+	return (int)round(pow(CONFIDENCE,2.0) * std * (1-std) / pow(ACCURACY,2.0)); 
+}
 
